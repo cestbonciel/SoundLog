b68473cb41c46cf2ce3c9b76daea1570ec2fae36
Kim <cestbonciel88@knou.ac.kr>
2023-10-09 23:44:49 +0900

    Remove: HomeCollectionsViewController.swift file

--- SoundLog/SoundLog/Presentation/HomeScene/HomeViewController.swift
+++ SoundLog/SoundLog/Presentation/HomeScene/HomeViewController.swift
@@ -6,31 +6,44 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeViewController: UIViewController {
 
-	
+	@IBOutlet weak var topSideStackView: UIStackView!
 	@IBOutlet weak var monthlyArchive: UILabel!
 	
 	@IBOutlet weak var AddButton: UIButton!
+	
+	private lazy var calendarView: UICalendarView = {
+		let calendarView = UICalendarView()
+		let gregorianCalendar = Calendar(identifier: .gregorian)
+		calendarView.calendar = gregorianCalendar
+		calendarView.locale = Locale(identifier: "ko-KR")
+		calendarView.fontDesign = .default
+		return calendarView
+	}()
+	
 	override func viewDidLoad() {
-        super.viewDidLoad()
-
+		super.viewDidLoad()
 		
-    }
-    
+		setupHomeUI()
+	}
+	
 	@IBAction func recordingSoundButtonTapped(_ sender: Any) {
 		let viewController = SoundLogViewController()
 		viewController.isModalInPresentation = true
 		viewController.modalPresentationStyle = .fullScreen
 		self.present(viewController, animated: true)
 	}
-//	recordingSound
-	/*
-	 @IBAction func actToOrange(_ sender: Any) {
-		 if let orangeVC = self.storyboard?.instantiateViewController(identifier: "Orange") {
-			 self.navigationController?.pushViewController(orangeVC, animated: true)
-		 }
-	 }
-	 */
+	
+//	private lazy var calendarView = UICalendarView()
+	private func setupHomeUI() {
+		view.addSubview(topSideStackView)
+		view.addSubview(calendarView)
+		calendarView.translatesAutoresizingMaskIntoConstraints = false
+		
+		
+	}
+	
 }
