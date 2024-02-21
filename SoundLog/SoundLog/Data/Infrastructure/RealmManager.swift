058b0fd4a0e261af457809e63bc5a655fce05b0f
Nat <cestbonciel88@knou.ac.kr>
2024-02-22 06:16:56 +0900

    [Feat]: setting up CRUD in Realm for the first time

--- SoundLog/SoundLog/Data/Infrastructure/RealmManager.swift
+++ SoundLog/SoundLog/Data/Infrastructure/RealmManager.swift
@@ -9,6 +9,51 @@ import Foundation
 import RealmSwift
 
 final class RealmManager {
+    static let shared = RealmManager()
+    private let realm: Realm
     
+    init() {
+        do {
+            realm = try Realm()
+        } catch {
+            fatalError("Error initializing Realm: \(error)")
+        }
+    }
+    
+    func saveObject<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.add(object)
+            }
+        } catch {
+            print("Error saving object to Realm: \(error)")
+        }
+    }
+    
+    func getAllObjects<T: Object>(_ objectType: T.Type) -> Results<T>? {
+        return realm.objects(objectType)
+    }
+    
+    func updateObject<T: Object>(_ object: T, with dictionary: [String: Any?]) {
+        do {
+            try realm.write {
+                for (key, value) in dictionary {
+                    object.setValue(value, forKey: key)
+                }
+            }
+        } catch {
+            print("Error updating object in Realm: \(error)")
+        }
+    }
+    
+    func deleteObject<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.delete(object)
+            }
+        } catch {
+            print("Error deleting object from Realm: \(error)")
+        }
+    }
     
 }
