bd9bc5c892098b4e793562b18a8201f993861598
Nat <cestbonciel88@knou.ac.kr>
2024-02-22 05:07:11 +0900

    [Feat]: Data mdel and divided Data folders, install Realm

---
+++ SoundLog/SoundLog/Data/Infrastructure/RealmManager.swift
@@ -0,0 +1,8 @@
+//
+//  RealmManager.swift
+//  SoundLog
+//
+//  Created by Nat Kim on 2024/02/22.
+//
+
+import Foundation
---
+++ SoundLog/SoundLog/Data/PersistentStorage/StorageSoundLog.swift
@@ -0,0 +1,8 @@
+//
+//  StorageSoundLog.swift
+//  SoundLog
+//
+//  Created by Nat Kim on 2024/02/22.
+//
+
+import Foundation
