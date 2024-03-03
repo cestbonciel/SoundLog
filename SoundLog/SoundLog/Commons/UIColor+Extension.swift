b27cfd2b0804a863f1bde26a184891283cf45694
Nat <cestbonciel88@knou.ac.kr>
2024-03-04 04:36:06 +0900

    Feat: add audio Player UIView codes

--- SoundLog/SoundLog/Commons/UIColor+Extension.swift
+++ SoundLog/SoundLog/Commons/UIColor+Extension.swift
@@ -25,50 +25,11 @@ extension UIColor {
 		return UIColor(red: 176.0 / 255.0, green: 176.0 / 255.0, blue: 176.0 / 255.0, alpha: 1)
 	}
 	
+    class var bgGray: UIColor {
+        return UIColor(red: 176.0 / 255.0, green: 176.0 / 255.0, blue: 176.0 / 255.0, alpha: 1)
+    }
+    
 	class var lightGray: UIColor {
 		return UIColor(red: 204.0/255.0, green: 204.0/255.0, blue: 204.0/255.0, alpha: 1)
 	}
 }
-//MARK: - Custom Font
-/*
-enum SoundLogCustomFontGM {
-	case GMSansLight
-	case GMSansMedium
-	case GMSansBold
-}
-
-
-
-extension NSAttributedString {
-	class func attributeFont(font: SoundLogCustomFontGM, size: CGFloat, text: String, lineHeight: CGFloat) -> NSAttributedString {
-		let attributeString = NSMutableAttributedString(string: text)
-		let paragraphStyle = NSMutableParagraphStyle()
-		
-		if #available(iOS 14.0, *) {
-			paragraphStyle.lineBreakStrategy = .hangulWordPriority
-		} else {
-			paragraphStyle.lineBreakStrategy = .pushOut
-		}
-		
-		var settingfont = UIFont()
-		switch font {
-		case .GMSansLight:
-			settingfont = UIFont(name: "GmarketSansLight", size: size)!
-		case .GMSansMedium:
-			settingfont = UIFont(name: "GmarketSansMedium", size: size)!
-		case .GMSansBold:
-			settingfont = UIFont(name: "GmarketSansBold", size: size)!
-		}
-		
-		paragraphStyle.lineSpacing = lineHeight - settingfont.lineHeight
-		
-		attributeString.addAttributes([
-					NSAttributedString.Key.paragraphStyle : paragraphStyle,
-					.font : settingfont
-				], range: NSMakeRange(0, attributeString.length))
-		
-		return attributeString
-	}
-
-}
-*/
---
+++ SoundLog/SoundLog/Presentation/ContentViews/CustomPlayerView.swift
@@ -0,0 +1,196 @@
+//
+//  CustomPlayerView.swift
+//  SoundLog
+//
+//  Created by Nat Kim on 2024/03/04.
+//
+
+import UIKit
+import AVFoundation
+import SnapKit
+
+class CustomPlayerView: UIView {
+    // 추가된 부분: 플레이어 상태 열거형
+    enum PlayerState {
+        case playing, paused
+    }
+    
+    // 추가된 부분: 현재 플레이어 상태
+    var currentPlayerState: PlayerState = .paused {
+        didSet {
+            setButtonState()
+        }
+    }
+    
+    var audioPlayer: AVAudioPlayer?
+    var audioTimer: Timer?
+    
+    init() {
+        super.init(frame: .zero)
+        setupUI()
+        queueSound()
+        makeTimer()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private lazy var playerBGView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.systemDimGray.withAlphaComponent(0.4)
+        view.layer.borderWidth = 2
+        view.layer.borderColor = UIColor.bgGray.cgColor
+        view.layer.cornerRadius = 10
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private lazy var playAndPauseBtn: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        button.setPreferredSymbolConfiguration(.init(pointSize: 16, weight: .regular, scale: .default), forImageIn: .normal)
+
+        button.tintColor = .black
+        button.addTarget(self, action: #selector(playbuttonTapped), for: .touchUpInside)
+        return button
+    }()
+   
+    private var audioPlayTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "00:00"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = UIColor.black
+        
+        return label
+    }()
+    
+
+    private lazy var playStateBar: UIProgressView = {
+        let progress = UIProgressView()
+        progress.setProgress(0.0, animated: true)
+        progress.tintColor = UIColor.neonPurple
+        progress.trackTintColor = UIColor.white
+        return progress
+    }()
+    
+    private func setButtonState() {
+
+        switch currentPlayerState {
+        case .playing:
+            playAndPauseBtn.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        case .paused:
+            playAndPauseBtn.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        }
+    }
+
+    
+    @objc private func playbuttonTapped() {
+
+        if currentPlayerState == .playing {
+            currentPlayerState = .paused
+            audioPlayer?.pause()
+        } else {
+            currentPlayerState = .playing
+            audioPlayer?.play()
+        }
+    }
+    
+    private func makeTimer() {
+        if audioTimer != nil {
+            audioTimer!.invalidate()
+        }
+        audioTimer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(updatePlayTime),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    private func queueSound() {
+
+        if let contentURL = Bundle.main.path(forResource: "Wood Fire", ofType: "wav") {
+            let url = URL(fileURLWithPath: contentURL)
+            do {
+                self.audioPlayer = try AVAudioPlayer(contentsOf: url)
+                audioPlayer?.delegate = self
+                audioPlayer?.volume = 1.0
+                audioPlayer?.prepareToPlay() // 추가된 부분: 사전에 오디오 준비
+            } catch {
+                print("오디오 플레이어 생성 오류: \(error)")
+            }
+        } else {
+            print("Error: Audio file not found.")
+        }
+    }
+
+
+    
+    @objc private func updatePlayTime() {
+        guard let player = audioPlayer else { return }
+        audioPlayTimeLabel.text = formatTimeInterval2String(player.currentTime)
+        playStateBar.progress = Float(player.currentTime / player.duration)
+    }
+    
+    private func formatTimeInterval2String(_ time: TimeInterval) -> String {
+        let mins = Int(time / 60)
+        let secs = Int(time.truncatingRemainder(dividingBy: 60))
+        let formattedTime = String(format: "%02d:%02d", mins, secs)
+        return formattedTime
+    }
+    
+    
+    
+    private func setupUI() {
+        let title = UILabel()
+        self.addSubview(title)
+        title.text = "소리의 기록"
+        title.textColor = .black
+        title.adjustsFontForContentSizeCategory = true
+        title.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(playerBGView)
+        playerBGView.addSubview(playAndPauseBtn)
+        playerBGView.addSubview(audioPlayTimeLabel)
+        playerBGView.addSubview(playStateBar)
+        title.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(64)
+        }
+        
+        playerBGView.snp.makeConstraints {
+            $0.top.equalTo(title.snp.bottom).offset(160)
+            $0.left.right.equalToSuperview().inset(24)
+            $0.height.equalTo(32)
+        }
+        
+        playAndPauseBtn.snp.makeConstraints {
+            $0.centerY.equalTo(playerBGView.snp.centerY)
+            $0.left.equalToSuperview().inset(16)
+        }
+        
+        audioPlayTimeLabel.snp.makeConstraints {
+            $0.centerY.equalTo(playerBGView.snp.centerY)
+            $0.left.equalTo(playAndPauseBtn.snp.right).offset(8)
+        }
+        
+        playStateBar.snp.makeConstraints {
+            $0.centerY.equalTo(playerBGView.snp.centerY)
+            $0.left.equalTo(audioPlayTimeLabel.snp.right).offset(8)
+            $0.right.equalToSuperview().inset(16)
+        }
+    }
+}
+
+extension CustomPlayerView: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if flag {
+            currentPlayerState = .paused
+            setButtonState()
+        } else {
+            print("Error: Audio playback finished unsuccessfully.")
+        }
+    }
+}
